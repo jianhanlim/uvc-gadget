c7b789a9563deb013d73a5583fb0e2138dde8ed1
Jian Han Lim <jianhanl@hotmail.com>
2024-09-11 17:10:13 +0800

    Update libcamera-source.cpp, adjust controls

--- lib/libcamera-source.cpp
+++ lib/libcamera-source.cpp
@@ -613,19 +613,23 @@ struct video_source *libcamera_source_create(const char *devname)
             std::cout << "Enabling auto white balance" << std::endl;
             src->controls.set(controls::AwbEnable, true);
         }
-	//if (infoMap.find(&controls::Brightness) != infoMap.end()) {
-        //    std::cout << "Enabling Brightness" << std::endl;
-        //    src->controls.set(controls::Brightness, 0.2);
-        //}
+	if (infoMap.find(&controls::Brightness) != infoMap.end()) {
+            std::cout << "Enabling Brightness" << std::endl;
+            src->controls.set(controls::Brightness, 0.01);
+        }
+	if (infoMap.find(&controls::Contrast) != infoMap.end()) {
+            std::cout << "Setting custom constrast" << std::endl;
+            src->controls.set(controls::Contrast, 1.01); // Example value in microseconds
+        }
 	// Set a specific exposure time
 	if (infoMap.find(&controls::ExposureTime) != infoMap.end()) {
 	    std::cout << "Setting custom exposure time" << std::endl;
-	    src->controls.set(controls::ExposureTime, 50000); // Example value in microseconds
+	    src->controls.set(controls::ExposureTime, 100000); // Example value in microseconds
 	}
 	// Adjust the analogue gain
 	if (infoMap.find(&controls::AnalogueGain) != infoMap.end()) {
 	    std::cout << "Setting analogue gain" << std::endl;
-	    src->controls.set(controls::AnalogueGain, 2.0); // Adjust gain, 2.0 is just an example
+	    src->controls.set(controls::AnalogueGain, 4.0); // Adjust gain, 2.0 is just an example
 	}
 	if (infoMap.find(&controls::draft::NoiseReductionMode) != infoMap.end()) {
 	    std::cout << "Enabling noise reduction" << std::endl;
